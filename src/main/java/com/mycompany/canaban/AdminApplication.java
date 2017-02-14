5dac0a802d324a09e2f387dce83ce22e1072cdf1
antongusev <gucebanton@gmal.com>
2017-02-15 00:09:09 +0300

    fix

--- src/main/java/com/mycompany/canaban/AdminApplication.java
+++ src/main/java/com/mycompany/canaban/AdminApplication.java
@@ -4,12 +4,10 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
 
 @EnableAdminServer
 @EnableDiscoveryClient
-@Configuration
 @EnableAutoConfiguration
 public class AdminApplication {
 
