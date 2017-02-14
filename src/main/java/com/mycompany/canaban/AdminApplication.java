f3b388aa6c242f6d7dcd467449b07cb2af448c7d
antongusev <gucebanton@gmal.com>
2017-02-15 00:46:08 +0300

    chng

--- src/main/java/com/mycompany/canaban/AdminApplication.java
+++ src/main/java/com/mycompany/canaban/AdminApplication.java
@@ -2,6 +2,7 @@ package com.mycompany.canaban;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -9,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableAdminServer
 @EnableDiscoveryClient
 @EnableAutoConfiguration
+@SpringBootConfiguration
 public class AdminApplication {
 
 	public static void main(String[] args) {
